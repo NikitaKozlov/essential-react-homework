d584a0d52db5b79a7dab5874e76fd88f3d17846d
Nikita Kozlov <nikita.kozlov@getyourguide.com>
2026-04-24 13:00:50 +0200

    add FAB

---
+++ web/src/components/AddLotteryButton.tsx
@@ -0,0 +1,10 @@
+import { Fab, Icon } from "@mui/material";
+
+export default function AddLotteryButton() {
+  return (
+    <Fab variant="extended" className="add-lottery-button">
+      <Icon>add</Icon>
+      ADD LOTTERY
+    </Fab>
+  );
+}
